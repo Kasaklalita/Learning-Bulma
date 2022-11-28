e726202faa9013b67fd68d2f31f939c42a021b30
Veniamin Polienko <kasaklalita@mail.ru>
2022-11-28 10:47:55 +0300

    Created a model for Product and made migrations

---
+++ bulma_backend/api/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    slug = models.SlugField()
+    description = models.TextField(blank=True, null=True)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    image = models.ImageField(upload_to='uploads/', blank=True, null=True)
+    thumbnail = models.ImageField(upload_to='uploads/', blank=True, null=True)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ('-date_added',)
+
+    def __str__(self):
+        return self.name
+
+    def get_image(self):
+        if self.image:
+            return f'http://127.0.0.1:5500/bulma_frontend/' + self.image.url
+        return ''
+
+
